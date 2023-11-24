665991e3b5a88fc6e7825534a9d476310d3b3493
JulyannaGama <julyannagama6@gmail.com>
2023-11-24 14:44:20 -0300

    Tarefa 3

--- script.js
+++ script.js
@@ -12,49 +12,71 @@ const tasks = [
 ];
 
 function createTaskItem(task) {
-
   const list = document.createElement("li");
-  const inforList = document.createElement("div");
+  const div = document.createElement("div");
   const urgencyLevel = document.createElement("span");
   const taskTitle = document.createElement("p");
-  const button = document.createElement('button')
-  const img = document.createElement('img')
+  const button = document.createElement('button');
+  const img = document.createElement('img');
 
   list.classList.add("task__item");
-  inforList.classList.add('task-info__container');
+  div.classList.add('task-info__container');
   urgencyLevel.classList.add("task-type");
   taskTitle.innerText = task.title;
   button.classList.add('task__button--remove-task');
 
-  if (task.type === 'Urgente') {
+  if (task.type.toLowerCase() === 'urgente') {
     urgencyLevel.classList.add('span-urgent')
-  }
-  if (task.type === 'Importante') {
+  };
+  if (task.type.toLowerCase() === 'importante') {
     urgencyLevel.classList.add('span-important')
-  }
-  if (task.type === 'Normal') {
+  };
+  if (task.type.toLowerCase() === 'normal') {
     urgencyLevel.classList.add('span-normal')
-  }
+  };
 
-  img.src = "./assets/trash-icon.svg"
-  img.alt = "Lixeira"
-
-  inforList.appendChild(urgencyLevel);
-  inforList.appendChild(taskTitle);
-  list.appendChild(inforList);
-  list.appendChild(button);
-  button.appendChild(img)
+  img.src = "./assets/trash-icon.svg";
+  img.alt = "Lixeira";
+  
+  list.append(div, button);
+  div.append(urgencyLevel, taskTitle);
+  button.appendChild(img);
 
   return list;
 }
 
 function renderElements(array) {
   const taskUl = document.querySelector('.tasks__list');
-  taskUl.innerHTML = ''
+  taskUl.innerHTML = '';
   for (let i = 0; i < array.length; i++) {
     const allTasks = array[i];
     const tasksItem = createTaskItem(allTasks);
-    taskUl.appendChild(tasksItem)
+    taskUl.appendChild(tasksItem);
   }
 }
 renderElements(tasks)
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const buttonAddTask = document.querySelector('.form__button--add-task');
+
+  buttonAddTask.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const titleTask = document.querySelector('.form__input--text');
+    const urgency = document.querySelector('.form__input--priority');
+
+    const newTask = {
+      title: titleTask.value,
+      type: urgency.value
+    };
+
+    if (titleTask.value.length == 0 || urgency.value.length == 0) {
+      alert('Algum campo ficou vazio.')
+    } else {
+      tasks.push(newTask);
+      renderElements(tasks)
+    };
+
+  });
+});
